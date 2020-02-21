573d3cf38676dc3837179cdbe1b4f67d1849e6a2
sratcha <sratcha.sae@gmail.com>
2020-02-21 11:54:39 +0700

    update checkout

--- src/modules/Checkout/CheckoutView.js
+++ src/modules/Checkout/CheckoutView.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     View,
     Text,
     Image,
     StyleSheet,
     ImageBackground,
-    Linking,
 } from 'react-native';
-import { Appbar, Checkbox } from 'react-native-paper';
+import { Appbar, TextInput } from 'react-native-paper';
 import { fonts, colors } from '../../styles';
-import { Button } from '../../components';
+import { Button, Dropdown } from '../../components';
+import ImagePicker from 'react-native-image-picker';
+
 
 export default function CheckoutScreen(props) {
-    state = {
-        checked: false,
+
+    const [filePath, setFilePath] = useState({});
+    const chooseFile = () => {
+        var options = {
+            title: 'Select Image',
+            customButtons: [
+                { name: 'customOptionKey', title: 'Choose Photo from Custom Option' },
+            ],
+            storageOptions: {
+                skipBackup: true,
+                path: 'images',
+            },
+        };
+        ImagePicker.showImagePicker(options, response => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+                alert(response.customButton);
+            } else {
+                let source = response;
+                setFilePath(source);
+            }
+        });
     };
-    const { checked } = this.state;
+
+    const [text, setText] = useState(false);
+
     return (
-        <View >
+        <View>
             <Appbar.Header >
                 <Appbar.BackAction
                     onPress={() => props.navigation.goBack()}
@@ -27,29 +56,50 @@ export default function CheckoutScreen(props) {
                     title="Check Out"
                 />
             </Appbar.Header>
-            <Checkbox
-                status={checked ? 'checked' : 'unchecked'}
-                onPress={() => { this.setState({ checked: !checked }); }}
-            >
-            </Checkbox>
+            <View style={{ paddingTop: 20}}>
+            <Dropdown
+                style={{ width: 200, alignSelf: 'center' }}
+                onSelect={() => { }}
+                items={['เสร็จ', 'ไม่เสร็จ']}
+            />
+            </View>
             <View style={styles.componentsSection}>
-                {/* <Text style={styles.componentSectionHeader}></Text> */}
-
-                {/* <View style={styles.demoButtonsContainer}> */}
-                <Button
-                    style={styles.demoButton}
-                    primary
-                    caption="OK"
-                    onPress={() => props.navigation.navigate({ routeName: 'GridScreen' })}
+                <Image
+                    source={{
+                        uri: 'data:image/jpeg;base64,' + filePath.data,
+                    }}
+                    style={{ width: 150, height: 150 }}
                 />
-                <Button
-                    style={styles.demoButton}
-                    secondary
-                    caption="Cencel"
-                    onPress={() => props.navigation.goBack()}
+                <Image
+                    source={{ uri: filePath.uri }}
+                    style={{ width: 150, height: 150 }}
                 />
 
             </View>
+            <View style={styles.demoButton}>
+                <Button caption="Cencel" onPress={chooseFile} />
+            </View>
+            {/* </View> */}
+
+            {/* <View> */}
+            <TextInput
+                label="comment"
+                value={text}
+                onChangeText={text => setText({ text })}
+            ></TextInput>
+            {/* </View>
+            <View > */}
+
+            <Button
+                style={styles.button}
+                primary
+                caption="OK"
+                // onPress={() => props.navigation.navigate({ routeName: 'GridScreen' })}
+                onPress={() => props.navigation.goBack()}
+            />
+
+            {/* </View> */}
+
             {/* </View> */}
         </View>
     );
@@ -58,7 +108,7 @@ export default function CheckoutScreen(props) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'flex-start',
+        alignItems: 'center',
         paddingHorizontal: 30,
         paddingVertical: 50,
         justifyContent: 'space-around',
@@ -77,16 +127,16 @@ const styles = StyleSheet.create({
         alignItems: 'flex-start',
     },
     buttonsContainer: {
-        flexDirection: 'row',
+        // flexDirection: 'row',
         alignItems: 'center',
         alignSelf: 'stretch',
         // paddingTop: 50,
         color: '#009e73'
     },
     button: {
-        alignSelf: 'stretch',
+        marginTop: 90,
+        paddingHorizontal: 20,
         marginBottom: 20,
-        color: '#009e73'
     },
     componentsSection: {
         backgroundColor: colors.white,
@@ -94,7 +144,7 @@ const styles = StyleSheet.create({
         padding: 15,
         marginBottom: 20,
         borderRadius: 10,
-        paddingTop: '100%',
+        paddingTop: 20,
         alignItems: 'center',
         justifyContent: 'space-between',
     },
@@ -113,7 +163,7 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
     },
     demoButton: {
-        marginTop: 50,
+        marginTop: 20,
         paddingHorizontal: 12,
         marginBottom: 8,
         // width: 40,
@@ -121,4 +171,20 @@ const styles = StyleSheet.create({
     demoItem: {
         marginVertical: 15,
     },
+    container1: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+// import React from 'react';
+// import { StyleSheet, Text, View, Button, Image } from 'react-native';
+// export default class App extends React.Component {
+
+//   render() {
+// return (
+
+//     );
+//   }
+// }
